59b525aa85a24695459917b8b2e99cf71aef4487
danstrom419 <dll9@sunyorange.edu>
2024-11-22 14:28:29 -0500

    Add files via upload

---
+++ TempConverter.py
@@ -0,0 +1,27 @@
+
+
+#Temperature convertor: F to C
+def f_to_c():
+    farenheight = input("Please enter the temperature in Farenheight: ")
+    celsius = (int(farenheight)-32) * (5/9)
+    print(celsius)
+
+#Temperature convertor C to F
+def c_to_f():
+    celsius = input("Please enter the temperature in Celsius: ")
+    farenheight = ((int(celsius)*(9/5)) + 32)
+    print(farenheight)
+
+#Gives user input to choose temp they want to convert to
+while True:
+    type_of_conversion = input("What temperature are you want to convert to? (C/F): ").upper()
+    if type_of_conversion == "F":
+        f_to_c()
+        break
+    elif type_of_conversion == "C":
+        c_to_f()
+        break
+    else:
+        print("Please enter a C or F")
+
+
